83e042573fc2866870509fa97557de5405e17b95
Kyungjik Min <dp.min@lge.com>
2016-02-11 10:50:29 +0900

    Add mkcscope script
    Change-Id: I913bce7f95b2f5f5ac2fa470f128dcdc6f5a36c6

--- vim-setting.sh
+++ vim-setting.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 
 # made by kyungjik.min
-# ver 0.1
+# ver 0.2
 
 echo "Step#1. install vim ..."
 sudo apt-get install -y vim
 sleep 1
 
-echo "Step#2. install ctags ..."
+echo "\nStep#2. install ctags ..."
 sudo apt-get install -y ctags
 sleep 1
 
-echo "Step#3. download Vundle plugin ..."
+echo "\nStep#3. install cscope ..."
+sudo apt-get install -y cscope
+sleep 1
+
+echo "\nStep#4. download Vundle plugin ..."
 bundle_dir=".vim/bundle"
 if [ ! -d ~/$bundle_dir ] 
 then mkdir ~/$bundle_dir
@@ -19,10 +23,14 @@ fi
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 sleep 1
 
-echo "Step#4. install plugins ..."
+echo "\nStep#5. install plugins ..."
 vim +PluginInstall +qall
 sleep 1
 
-echo "Step#5. copy vimrc ..."
-mv .vimrc ~/.vimrc
+echo "\nStep#6. copy vimrc ..."
+cp .vimrc ~/.vimrc
+sleep 1
+
+echo "\nStep#7. copy mkcscope ..."
+sudo cp mkcscope.sh /usr/bin/
 sleep 1
