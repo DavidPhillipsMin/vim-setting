bb17ebbbc55070a3dd12778479ed37a2cb2feb58
Kyungjik Min <dp.min@lge.com>
2016-02-15 17:14:53 +0900

    Add check routine of needed package list.
    Change-Id: Ibe81ecf63311f5d8fbbc5d8f1bbcea83738ad72c

--- install.sh
+++ install.sh
@@ -5,33 +5,46 @@
 # ver 0.3
 
 echo "Step#1. install vim ..."
-sudo apt-get install -y vim
-sleep 1
+result=$(dpkg -l | grep vim | wc -l)
+if [ $result -eq 0 ]
+then
+    sudo apt-get install -y vim > /dev/null
+fi
+sleep 0.5
 
 echo "\nStep#2. install ctags ..."
-sudo apt-get install -y ctags
-sleep 1
+result=$(dpkg -l | grep ctags | wc -l)
+if [ $result -eq 0 ]
+then
+    sudo apt-get install -y ctags > /dev/null
+fi
+sleep 0.5
 
 echo "\nStep#3. install cscope ..."
-sudo apt-get install -y cscope
-sleep 1
+result=$(dpkg -l | grep cscope | wc -l)
+if [ $result -eq 0 ]
+then
+    sudo apt-get install -y cscope > /dev/null
+fi
+sleep 0.5
 
 echo "\nStep#4. download Vundle plugin ..."
 bundle_dir=".vim/bundle"
 if [ ! -d ~/$bundle_dir ] 
-then mkdir ~/$bundle_dir
+then
+    mkdir ~/$bundle_dir
 fi
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-sleep 1
+sleep 0.5
 
 echo "\nStep#5. install plugins ..."
 vim +PluginInstall +qall
-sleep 1
+sleep 0.5
 
 echo "\nStep#6. copy vimrc ..."
 cp .vimrc ~/.vimrc
-sleep 1
+sleep 0.5
 
 echo "\nStep#7. copy mkcscope ..."
 sudo cp mkcscope.sh /usr/bin/
-sleep 1
+sleep 0.5
